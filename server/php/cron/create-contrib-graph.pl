jjhursey/mtt-modern

server: Misc changes necessary to port to AWS from IU servers
 * cherrypy: Disable global trial flag
 * Switch postgres serialization option from depricated value
 * reporter: PHP 7 replace split
 * CherryPy misc
   - Add a Get for the /submit interface for debugging
   - Some documentation in the main MTTServer.py
 * Rename config script to template so we don't accidentally commit it
 * Add date string to debug output
 * Add downtime pages, for quick reference
 * Add a helpful error message when gunzip fails
 * Move to using a config file for the cron perl scripts (Add tmpl)

Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

server/php/cron/create-contrib-graph.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use Env qw(HOME PATH USER);
+use Config::IniFiles;
 
 #
 # Script requires the following software packages installed:
@@ -10,12 +11,8 @@ use Env qw(HOME PATH USER);
 # - ps2pdf
 #
 
-# Directory containing scripts to execute
-my $working_dir = "/l/osl/www/mtt.open-mpi.org/mtt/server/php/cron/stats";
-# Directory to place the contribution graph
-my $output_dir  = "/l/osl/www/mtt.open-mpi.org/mtt/server/php/stats/";
-# Temporary directory to store data files
-my $tmp_dir = "/tmp/";
+my $config_filename = "config.ini";
+my $ini_section;
 
 my $cmd;
 
@@ -39,6 +36,26 @@ if( 0 != parse_cmd_line() ) {
   print_usage();
   exit -1;
 }
+
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $filename\n";
+    exit 1;
+}
+
+# Check the contents of the config file
+check_ini_section($ini, "stats", ("working_dir", "output_dir", "tmp_dir") );
+
+$ini_section = "stats";
+# Directory containing scripts to execute
+my $working_dir = resolve_value($ini, $ini_section, "working_dir");
+# Directory to place the contribution graph
+my $output_dir  = resolve_value($ini, $ini_section, "output_dir");
+# Temporary directory to store data files
+my $tmp_dir = resolve_value($ini, $ini_section, "tmp_dir");
+
 
 if(!chdir($working_dir) ) {
   print "Error: Cannot chdir to <$working_dir>\n";
@@ -139,6 +156,15 @@ sub parse_cmd_line() {
     elsif( $ARGV[$i] eq "-l" ) {
       $is_limited_to_one_year = "t";
     }
+    elsif( $ARGV[$i] =~ /-config/ ) {
+      $i++;
+      if( $i < $argc ) {
+        $config_filename = $ARGV[$i];
+      } else {
+        print_update("Error: -config requires a file argument\n");
+        return -1;
+      }
+    }
     #
     # Invalid options produce a usage message
     #
@@ -157,4 +183,49 @@ sub print_usage() {
   print "="x50 . "\n";
 
   return 0;
+}
+
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
 }

server/php/cron/ic-check.pl
@@ -10,6 +10,7 @@ use strict;
 use DBI;
 use Mail::Sendmail;
 use Class::Struct;
+use Config::IniFiles;
 
 # Flush I/O frequently
 $| = 1;
@@ -18,10 +19,32 @@ my $verbose;
 my $debug;
 my $debug_no_email;
 
-#my $to_email_address     = "FILL THIS IN";
-#my $from_email_address   = "FILL THIS IN";
-my $to_email_address     = "mtt-devel-core\@lists.open-mpi.org";
-my $from_email_address   = "mtt-devel-core\@lists.open-mpi.org";
+my $config_filename = "config.ini";
+my $ini_section;
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $filename\n";
+    exit 1;
+}
+# Check the contents of the config file
+check_ini_section($ini, "database", ("user", "password", "hostname", "port", "dbname") );
+check_ini_section($ini, "reporting", ("to_email", "from_email") );
+
+# Read in config entries
+$ini_section = "database";
+my $mtt_user = resolve_value($ini, $ini_section, "user");;
+my $mtt_pass = resolve_value($ini, $ini_section, "password");
+my $mtt_hostname = resolve_value($ini, $ini_section, "hostname");
+my $mtt_port = resolve_value($ini, $ini_section, "port");
+my $mtt_dbname = resolve_value($ini, $ini_section, "dbname");
+
+$ini_section = "reporting";
+my $to_email_address     = resolve_value($ini, $ini_section, "to_email");
+my $from_email_address   = resolve_value($ini, $ini_section, "from_email");
+
+# Static values
 my $current_mail_subject = "MTT Database Maintenance: IC Check";
 my $current_mail_header  = "";
 my $current_mail_body    = "";
@@ -210,10 +233,9 @@ sub sql_scalar_stmt() {
 }
 
 sub connect_db() {
-  my $mtt_user = "mtt";
   my $stmt;
 
-  $dbh_mtt = DBI->connect("dbi:Pg:dbname=mtt",  $mtt_user);
+  $dbh_mtt = DBI->connect("dbi:Pg:dbname=".$mtt_dbname.";host=".$mtt_hostname.";port=".$mtt_port,  $mtt_user, $mtt_pass);
 
   $stmt = $dbh_mtt->prepare("set sort_mem = '512MB'");
   $stmt->execute();
@@ -320,4 +342,48 @@ sub send_status_mail() {
   }
 
   return 0;
+}
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
 }

server/php/cron/mtt-update-stats-db-only.pl
@@ -2,14 +2,27 @@
 
 use strict;
 use Env qw(HOME PATH USER);
+use Config::IniFiles;
 
 # Perform flush after each write to STDOUT
 $| = 1;
 
+my $config_filename = "config.ini";
+my $ini_section;
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $filename\n";
+    exit 1;
+}
+check_ini_section($ini, "stats", ("base_dir", "tmp_dir") );
+$ini_section = "stats";
+
 #
 # MTT Working Dir to find scripts
 #
-my $mtt_base_dir = "/l/osl/www/mtt.open-mpi.org/mtt/server/php/cron/";
+my $mtt_base_dir = resolve_value($ini, $ini_section, "base_dir");
 
 #
 # Stats collection script
@@ -41,3 +54,48 @@ system("echo >> ". $stats_output);
 system("echo >> ". $stats_output);
 
 exit 0;
+
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
+}

server/php/cron/periodic-maintenance.pl
@@ -21,14 +21,15 @@
 use strict;
 use DBI;
 use Mail::Sendmail;
+use Config::IniFiles;
 
 my $debug;
 my $debug_no_email;
 
-#my $to_email_address     = "FILL THIS IN";
-#my $from_email_address   = "FILL THIS IN";
-my $to_email_address     = "mtt-devel-core\@lists.open-mpi.org";
-my $from_email_address   = "mtt-devel-core\@lists.open-mpi.org";
+my $config_filename = "config.ini";
+my $ini_section;
+
+# Static values
 my $current_mail_subject = "MTT Database Maintenance: ";
 my $current_mail_header  = "";
 my $current_mail_body    = "";
@@ -94,6 +95,29 @@ if( 0 != parse_args() ) {
   exit -1;
 }
 
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $filename\n";
+    exit 1;
+}
+# Check the contents of the config file
+check_ini_section($ini, "database", ("user", "password", "hostname", "port", "dbname") );
+check_ini_section($ini, "reporting", ("to_email", "from_email") );
+
+# Read in config entries
+$ini_section = "database";
+my $mtt_user = resolve_value($ini, $ini_section, "user");
+my $mtt_pass = resolve_value($ini, $ini_section, "password");
+my $mtt_hostname = resolve_value($ini, $ini_section, "hostname");
+my $mtt_port = resolve_value($ini, $ini_section, "port");
+my $mtt_dbname = resolve_value($ini, $ini_section, "dbname");
+
+$ini_section = "reporting";
+my $to_email_address     = resolve_value($ini, $ini_section, "to_email");
+my $from_email_address   = resolve_value($ini, $ini_section, "from_email");
+
 set_date_ranges();
 
 if( $MAIN_YEAR == $cur_main ) {
@@ -144,6 +168,15 @@ sub parse_args() {
     }
     elsif( $ARGV[$i] =~ /-year/ ) {
       $cur_main = $MAIN_YEAR;
+    }
+    elsif( $ARGV[$i] =~ /-config/ ) {
+      $i++;
+      if( $i < $argc ) {
+        $config_filename = $ARGV[$i];
+      } else {
+        print_update("Error: -config requires a file argument\n");
+        return -1;
+      }
     }
     else {
       print_update("Unknown ARG $i) <".$ARGV[$i].">\n");
@@ -263,14 +296,13 @@ sub periodic_wait() {
 }
 
 sub connect_db() {
-  my $mtt_user = "mtt";
   my $stmt;
 
   if( defined($debug) ) {
     return 0;
   }
 
-  $dbh_mtt = DBI->connect("dbi:Pg:dbname=mtt",  $mtt_user);
+  $dbh_mtt = DBI->connect("dbi:Pg:dbname=".$mtt_dbname.";host=".$mtt_hostname.";port=".$mtt_port,  $mtt_user, $mtt_pass);
 
   $stmt = $dbh_mtt->prepare("set vacuum_mem = ".(32 * 1024));
   $stmt->execute();
@@ -541,4 +573,48 @@ sub send_reminder() {
   }
 
   return 0;
+}
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
 }

emallove/mtt

server/php/cron/create-contrib-graph.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use Env qw(HOME PATH USER);
+use Config::IniFiles;
 
 #
 # Script requires the following software packages installed:
@@ -9,13 +10,8 @@ use Env qw(HOME PATH USER);
 # - gnuplot (with postscript terminal)
 # - ps2pdf
 #
-
-# Directory containing scripts to execute
-my $working_dir = "/l/osl/www/mtt.open-mpi.org/mtt/server/php/cron/stats";
-# Directory to place the contribution graph
-my $output_dir  = "/l/osl/www/mtt.open-mpi.org/mtt/server/php/stats/";
-# Temporary directory to store data files
-my $tmp_dir = "/tmp/";
+my $config_filename = "config.ini";
+my $ini_section;
 
 my $cmd;
 
@@ -40,6 +36,25 @@ if( 0 != parse_cmd_line() ) {
   exit -1;
 }
 
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $config_filename\n";
+    exit 1;
+}
+
+# Check the contents of the config file
+check_ini_section($ini, "stats", ("working_dir", "output_dir", "tmp_dir") );
+
+$ini_section = "stats";
+# Directory containing scripts to execute
+my $working_dir = resolve_value($ini, $ini_section, "working_dir");
+# Directory to place the contribution graph
+my $output_dir  = resolve_value($ini, $ini_section, "output_dir");
+# Temporary directory to store data files
+my $tmp_dir = resolve_value($ini, $ini_section, "tmp_dir");
+
 if(!chdir($working_dir) ) {
   print "Error: Cannot chdir to <$working_dir>\n";
   exit(-1);
@@ -61,6 +76,7 @@ if( $is_limited_to_one_year eq "t" ) {
 #
 # Gather the raw data
 #
+
 $cmd = "./make-raw-data.pl -year ".$extra_cmd_line_arg." > ".$tmp_dir.$data_file_year;
 if(0 != system($cmd) ) {
   print "Error: Cannot exec the command <$cmd>\n";
@@ -139,6 +155,15 @@ sub parse_cmd_line() {
     elsif( $ARGV[$i] eq "-l" ) {
       $is_limited_to_one_year = "t";
     }
+    elsif( $ARGV[$i] =~ /-config/ ) {
+      $i++;
+      if( $i < $argc ) {
+        $config_filename = $ARGV[$i];
+      } else {
+        print_update("Error: -config requires a file argument\n");
+        return -1;
+      }
+    }
     #
     # Invalid options produce a usage message
     #
@@ -157,4 +182,50 @@ sub print_usage() {
   print "="x50 . "\n";
 
   return 0;
+}
+
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+    my $value;
+    
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
 }

server/php/cron/ic-check.pl
@@ -10,6 +10,7 @@ use strict;
 use DBI;
 use Mail::Sendmail;
 use Class::Struct;
+use Config::IniFiles;
 
 # Flush I/O frequently
 $| = 1;
@@ -18,10 +19,32 @@ my $verbose;
 my $debug;
 my $debug_no_email;
 
-#my $to_email_address     = "FILL THIS IN";
-#my $from_email_address   = "FILL THIS IN";
-my $to_email_address     = "mtt-devel-core\@lists.open-mpi.org";
-my $from_email_address   = "mtt-devel-core\@lists.open-mpi.org";
+my $config_filename = "config.ini";
+my $ini_section;
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $config_filename\n";
+    exit 1;
+}
+# Check the contents of the config file
+check_ini_section($ini, "database", ("user", "password", "hostname", "port", "dbname") );
+check_ini_section($ini, "reporting", ("to_email", "from_email") );
+
+# Read in config entries
+$ini_section = "database";
+my $mtt_user = resolve_value($ini, $ini_section, "user");;
+my $mtt_pass = resolve_value($ini, $ini_section, "password");
+my $mtt_hostname = resolve_value($ini, $ini_section, "hostname");
+my $mtt_port = resolve_value($ini, $ini_section, "port");
+my $mtt_dbname = resolve_value($ini, $ini_section, "dbname");
+
+$ini_section = "reporting";
+my $to_email_address     = resolve_value($ini, $ini_section, "to_email");
+my $from_email_address   = resolve_value($ini, $ini_section, "from_email");
+
+# Static values
 my $current_mail_subject = "MTT Database Maintenance: IC Check";
 my $current_mail_header  = "";
 my $current_mail_body    = "";
@@ -210,10 +233,9 @@ sub sql_scalar_stmt() {
 }
 
 sub connect_db() {
-  my $mtt_user = "mtt";
   my $stmt;
 
-  $dbh_mtt = DBI->connect("dbi:Pg:dbname=mtt",  $mtt_user);
+  $dbh_mtt = DBI->connect("dbi:Pg:dbname=".$mtt_dbname.";host=".$mtt_hostname.";port=".$mtt_port,  $mtt_user, $mtt_pass);
 
   $stmt = $dbh_mtt->prepare("set sort_mem = '512MB'");
   $stmt->execute();
@@ -320,4 +342,49 @@ sub send_status_mail() {
   }
 
   return 0;
+}
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+    my $value;
+    
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
 }

server/php/cron/mtt-update-stats-db-only.pl
@@ -2,14 +2,27 @@
 
 use strict;
 use Env qw(HOME PATH USER);
+use Config::IniFiles;
 
 # Perform flush after each write to STDOUT
 $| = 1;
 
+my $config_filename = "config.ini";
+my $ini_section;
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $config_filename\n";
+    exit 1;
+}
+check_ini_section($ini, "stats", ("base_dir", "tmp_dir") );
+$ini_section = "stats";
+
 #
 # MTT Working Dir to find scripts
 #
-my $mtt_base_dir = "/l/osl/www/mtt.open-mpi.org/mtt/server/php/cron/";
+my $mtt_base_dir = resolve_value($ini, $ini_section, "base_dir");
 
 #
 # Stats collection script
@@ -41,3 +54,49 @@ system("echo >> ". $stats_output);
 system("echo >> ". $stats_output);
 
 exit 0;
+
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+    my $value;
+    
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
+}

server/php/cron/periodic-maintenance.pl
@@ -21,14 +21,15 @@
 use strict;
 use DBI;
 use Mail::Sendmail;
+use Config::IniFiles;
 
 my $debug;
 my $debug_no_email;
 
-#my $to_email_address     = "FILL THIS IN";
-#my $from_email_address   = "FILL THIS IN";
-my $to_email_address     = "mtt-devel-core\@lists.open-mpi.org";
-my $from_email_address   = "mtt-devel-core\@lists.open-mpi.org";
+my $config_filename = "config.ini";
+my $ini_section;
+
+# Static values
 my $current_mail_subject = "MTT Database Maintenance: ";
 my $current_mail_header  = "";
 my $current_mail_body    = "";
@@ -94,6 +95,29 @@ if( 0 != parse_args() ) {
   exit -1;
 }
 
+my $ini = new Config::IniFiles(-file => $config_filename,
+                               -nocase => 1,
+                               -allowcontinue => 1);
+if( !$ini ) {
+    print "Error: Failed to read: $config_filename\n";
+    exit 1;
+}
+# Check the contents of the config file
+check_ini_section($ini, "database", ("user", "password", "hostname", "port", "dbname") );
+check_ini_section($ini, "reporting", ("to_email", "from_email") );
+
+# Read in config entries
+$ini_section = "database";
+my $mtt_user = resolve_value($ini, $ini_section, "user");
+my $mtt_pass = resolve_value($ini, $ini_section, "password");
+my $mtt_hostname = resolve_value($ini, $ini_section, "hostname");
+my $mtt_port = resolve_value($ini, $ini_section, "port");
+my $mtt_dbname = resolve_value($ini, $ini_section, "dbname");
+
+$ini_section = "reporting";
+my $to_email_address     = resolve_value($ini, $ini_section, "to_email");
+my $from_email_address   = resolve_value($ini, $ini_section, "from_email");
+
 set_date_ranges();
 
 if( $MAIN_YEAR == $cur_main ) {
@@ -144,6 +168,15 @@ sub parse_args() {
     }
     elsif( $ARGV[$i] =~ /-year/ ) {
       $cur_main = $MAIN_YEAR;
+    }
+    elsif( $ARGV[$i] =~ /-config/ ) {
+      $i++;
+      if( $i < $argc ) {
+        $config_filename = $ARGV[$i];
+      } else {
+        print_update("Error: -config requires a file argument\n");
+        return -1;
+      }
     }
     else {
       print_update("Unknown ARG $i) <".$ARGV[$i].">\n");
@@ -263,14 +296,13 @@ sub periodic_wait() {
 }
 
 sub connect_db() {
-  my $mtt_user = "mtt";
   my $stmt;
 
   if( defined($debug) ) {
     return 0;
   }
 
-  $dbh_mtt = DBI->connect("dbi:Pg:dbname=mtt",  $mtt_user);
+  $dbh_mtt = DBI->connect("dbi:Pg:dbname=".$mtt_dbname.";host=".$mtt_hostname.";port=".$mtt_port,  $mtt_user, $mtt_pass);
 
   $stmt = $dbh_mtt->prepare("set vacuum_mem = ".(32 * 1024));
   $stmt->execute();
@@ -541,4 +573,49 @@ sub send_reminder() {
   }
 
   return 0;
+}
+
+sub resolve_value() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my $key = shift(@_);
+    my $value;
+    
+    $value = $ini->val($section, $key);
+    if( !defined($value) ) {
+        print "Error: Failed to find \"$key\" in section \"$section\"\n";
+        exit 1;
+    }
+    $value =~ s/^\"//;
+    $value =~ s/\"$//;
+
+    if( $value =~ /^run/ ) {
+        $value = $';
+        $value =~ s/^\(//;
+        $value =~ s/\)$//;
+        $value = `$value`;
+        chomp($value);
+    }
+
+    return $value;
+}
+
+sub check_ini_section() {
+    my $ini = shift(@_);
+    my $section = shift(@_);
+    my @keys = @_;
+
+    if( !$ini->SectionExists($section) ) {
+        print "Error: INI file does not contain a $section field\n";
+        exit 1;
+    }
+
+    foreach my $key (@keys) {
+        if( !$ini->exists($section, $key) ) {
+            print "Error: INI file missing $section key named $key\n";
+            exit 1;
+        }
+    }
+
+    return 0;
 }